c339263f95a5201cb935f8d3a53c8c1ce655964e
pyllyukko <pyllyukko@maimed.org>
2012-10-13 22:17:41 +0300

    cleanups.

--- harden.sh
+++ harden.sh
@@ -1814,9 +1814,9 @@ do
       remove_packages
       remove_shells
       import_pgp_keys
-      check_and_patch /etc "${ETC_PATCH_FILE}" 1 && ETC_CHANGED=1
-      check_and_patch /etc "${SUDOERS_PATCH_FILE}" 1
-      check_and_patch /etc/ssh "${SSH_PATCH_FILE}" 1
+      check_and_patch /etc	"${ETC_PATCH_FILE}"	1 && ETC_CHANGED=1
+      check_and_patch /etc	"${SUDOERS_PATCH_FILE}"	1
+      check_and_patch /etc/ssh	"${SSH_PATCH_FILE}"	1
 
       # this should be run after patching etc,
       # there might be new rc scripts.
