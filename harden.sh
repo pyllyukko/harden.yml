997a50fc973ef43f34c429c5dea5128cd74fe46e
pyllyukko <pyllyukko@maimed.org>
2015-10-16 17:09:01 +0300

    Added OpenVPN's signing key

--- harden.sh
+++ harden.sh
@@ -250,6 +250,7 @@ declare -ra PGP_URLS=(
 #   - 0x4623E8F745953F23 - http://deb.mempo.org/
 #   - 0x00CCB587DDBEF0E1 - The Irssi project <staff@irssi.org>
 #   - 1E453B2CE87BEE2F7DFE99661E34A1828E207901 - LEAP (https://bitmask.net/en/install/signature-verification)
+#   - 0xC29D97ED198D22A3 - https://openvpn.net/index.php/open-source/documentation/sig.html
 declare -ra PGP_KEYS=(
   #"CEA0A321"
   #"060798CB"
@@ -298,6 +299,7 @@ declare -ra PGP_KEYS=(
   "0x4623E8F745953F23"
   "0x00CCB587DDBEF0E1"
   "0x1E453B2CE87BEE2F7DFE99661E34A1828E207901"
+  "0xC29D97ED198D22A3"
 )
 # from CIS Apache HTTP Server 2.4 Benchmark v1.1.0 - 12-03-2013
 # 1.2.3-8
