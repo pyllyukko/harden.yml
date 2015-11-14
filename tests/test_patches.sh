29864b990118b1adb6c33d32c33d5cbe62964fd8
pyllyukko <pyllyukko@maimed.org>
2015-11-14 20:35:05 +0200

    Updated etc to revision 3

--- tests/test_patches.sh
+++ tests/test_patches.sh
@@ -60,7 +60,7 @@ fi
 mkdir -v tmp
 
 for PKG in \
-  "a/etc-14.2-${ARCH}-1.txz" \
+  "a/etc-14.2-${ARCH}-3.txz" \
   "n/network-scripts-14.1-noarch-2.txz" \
   'a/sysvinit-scripts-2.0-noarch-20.txz' \
   "a/sysvinit-functions-8.53-${ARCH}-2.txz" \
