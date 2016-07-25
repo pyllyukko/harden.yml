96d40fd414b4532945c7a8b4fe228a0113d7121e
pyllyukko <pyllyukko@maimed.org>
2016-07-25 19:08:28 +0300

    grub2-install note

--- harden.sh
+++ harden.sh
@@ -2139,8 +2139,9 @@ function configure_basic_auditing() {
   # enable it in grub/lilo
   if [ -f /etc/default/grub ] && ! grep -q '^GRUB_CMDLINE_LINUX=".*audit=1' /etc/default/grub
   then
+    # example: https://wiki.debian.org/AppArmor/HowToUse#Enable_AppArmor
     sed -i 's/^\(GRUB_CMDLINE_LINUX=".*\)"$/\1 audit=1"/' /etc/default/grub
-    echo "NOTICE: /etc/default/grub updated. you need to run \`update-grub' to update the boot loader."
+    echo "NOTICE: /etc/default/grub updated. you need to run \`update-grub' or \`grub2-install' to update the boot loader."
   elif [ -f /etc/lilo.conf ] && ! grep -q '^append=".*audit=1' /etc/lilo.conf
   then
     sed -i 's/^\(append=".*\)"$/\1 audit=1"/' /etc/lilo.conf
