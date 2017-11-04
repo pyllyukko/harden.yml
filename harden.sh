3e51a9f8ee12c164b331610391a72f40bcc9f493
pyllyukko <pyllyukko@maimed.org>
2017-11-04 12:47:13 +0200

    Added a TODO

--- harden.sh
+++ harden.sh
@@ -1715,6 +1715,7 @@ EOF
   else
     echo "[-] WARNING: sysctl.conf.new not found!" 1>&2
   fi
+  # TODO: make the settings effective
 } # sysctl_harden()
 ################################################################################
 function configure_tcp_wrappers() {
