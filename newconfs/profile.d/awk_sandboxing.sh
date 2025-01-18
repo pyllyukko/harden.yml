bbe4b4a1ee897857de3ead5f8521e5d45afd2104
pyllyukko <pyllyukko@maimed.org>
2025-01-18 23:20:13 +0200

    Yet another fix to awk_sandboxing.sh: mawk (tested for errors) -> awk

--- newconfs/profile.d/awk_sandboxing.sh
+++ newconfs/profile.d/awk_sandboxing.sh
@@ -4,7 +4,7 @@
 # created:      16-10-2011
 ################################################################################
 AWK_VERSION=`awk 'BEGIN{split(PROCINFO["version"],versinfo,".");print versinfo[1]}'`
-if mawk --version 2>/dev/null | grep -q '^GNU Awk' && [ -n "${AWK_VERSION}" ] && [ ${AWK_VERSION} -ge 4 ]
+if awk --version 2>/dev/null | grep -q '^GNU Awk' && [ -n "${AWK_VERSION}" ] && [ ${AWK_VERSION} -ge 4 ]
 then
   alias awk='/bin/gawk --sandbox'
 fi
