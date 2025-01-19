3f2eaa32910c38463c0f02c1a181f9a003a4c2e3
pyllyukko <pyllyukko@maimed.org>
2025-01-19 09:22:48 +0200

    awk_sandboxing.sh: Check that awk exists

--- newconfs/profile.d/awk_sandboxing.sh
+++ newconfs/profile.d/awk_sandboxing.sh
@@ -3,9 +3,12 @@
 # file:         awk_sandboxing.sh
 # created:      16-10-2011
 ################################################################################
-AWK_VERSION=$(awk 'BEGIN{split(PROCINFO["version"],versinfo,".");print versinfo[1]}')
-if awk --version 2>/dev/null | grep -q '^GNU Awk\b' && [ -n "${AWK_VERSION}" ] && [ "${AWK_VERSION}" -ge 4 ]
+if hash awk 2>/dev/null
 then
-  alias awk='/bin/gawk --sandbox'
+  AWK_VERSION=$(awk 'BEGIN{split(PROCINFO["version"],versinfo,".");print versinfo[1]}')
+  if awk --version 2>/dev/null | grep -q '^GNU Awk\b' && [ -n "${AWK_VERSION}" ] && [ "${AWK_VERSION}" -ge 4 ]
+  then
+    alias awk='/bin/gawk --sandbox'
+  fi
+  unset AWK_VERSION
 fi
-unset AWK_VERSION
