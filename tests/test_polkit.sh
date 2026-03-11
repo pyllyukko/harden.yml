e6ed9a28b17ec146492016b34ab83bc5d2563338
pyllyukko <pyllyukko@maimed.org>
2026-03-11 20:33:15 +0200

    Added a small polkit test

---
+++ tests/test_polkit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Used by run0
+action="org.freedesktop.systemd1.manage-units"
+
+if [ ${#} -ne 1 ] || [[ ! ${1} =~ ^[0-9]+$ ]]
+then
+  echo '[-] Error' 1>&2
+  exit 1
+fi
+
+/usr/bin/pkcheck --action-id="${action}" $$
+ret=${?}
+echo "[*] pkcheck returned ${ret}"
+if [ ${ret} -ne ${1} ]
+then
+  exit 1
+fi
