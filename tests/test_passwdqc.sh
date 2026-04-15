8216aac8e30774905153f922fe6c93c231eb8956
pyllyukko <pyllyukko@maimed.org>
2026-04-15 22:59:56 +0300

    Test that passwdqc reject bad passwords

---
+++ tests/test_passwdqc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ret=0
+
+# N0 is used for passwords consisting of characters from one character class only.
+# Lowercase alphabets only
+pass="$(tr -dc 'a-z' < /dev/urandom | head -c 30)"
+echo "[*] Password: ${pass}"
+echo "runner:${pass}" | sudo /usr/sbin/chpasswd
+if [ ${PIPESTATUS[1]} -ne 1 ]
+then
+  echo -e '[\033[1;31m-\033[0m] passwdqc did not reject bad passwd' 1>&2
+  ret=1
+fi
+
+# Alphabets only
+pass="$(tr -dc 'a-zA-Z' < /dev/urandom | head -c 23)"
+echo "[*] Password: ${pass}"
+echo "runner:${pass}" | sudo /usr/sbin/chpasswd
+if [ ${PIPESTATUS[1]} -ne 1 ]
+then
+  echo -e '[\033[1;31m-\033[0m] passwdqc did not reject bad passwd' 1>&2
+  ret=1
+fi
+
+exit ${ret}
