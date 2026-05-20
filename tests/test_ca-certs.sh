5f59abc283f5801b0f4d8a7e1f70ae79383d5745
pyllyukko <pyllyukko@maimed.org>
2026-05-20 23:16:49 +0300

    test_ca-certs.sh: Move the Outlook tests to the bottom
    So we get to test the other mail servers.
    
    Microsoft is still using certs signed by the DigiCert Global Root CA,
    which was removed from the ca-certificates-20260413 package 🤬

--- tests/test_ca-certs.sh
+++ tests/test_ca-certs.sh
@@ -92,15 +92,15 @@ done
 for host in \
   mx01.mail.icloud.com				\
   alt4.gmail-smtp-in.l.google.com		\
-  outlook-com.olc.protection.outlook.com	\
-  hotmail-com.olc.protection.outlook.com	\
   mta7.am0.yahoodns.net				\
   mail.cwo.com					\
   mail.protonmail.ch				\
   spool.mail.gandi.net				\
   fb.mail.gandi.net				\
   mx-tnl.mail.saunalahti.fi			\
-  mx-stp.mail.saunalahti.fi
+  mx-stp.mail.saunalahti.fi			\
+  outlook-com.olc.protection.outlook.com	\
+  hotmail-com.olc.protection.outlook.com
 do
   echo "[*] Testing SMTP STARTTLS for \`${host}'"
   openssl s_client -connect "${host}":25 -starttls smtp -verify_return_error -CAfile "${ca_file}" -showcerts 0</dev/null || exit 1
