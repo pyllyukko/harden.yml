30596c5e304d22e17eacf7d27abe2208474cdbed
pyllyukko <pyllyukko@maimed.org>
2026-05-20 21:43:42 +0300

    test_ca-certs.sh: Use Slackware's CA package if possible

--- tests/test_ca-certs.sh
+++ tests/test_ca-certs.sh
@@ -1,15 +1,63 @@
 #!/bin/bash
 
 ca_file=./ca-certificates.crt
+ca_dir="/usr/share/ca-certificates"
+echo "[*] Fetching Slackware's CA package version"
+slackware_ca="$(curl ftp://ftp.slackware.com/pub/slackware/slackware64-15.0/patches/packages/ | gawk '$9 ~ /^ca-certificates-[0-9]{8}-noarch-[0-9]+_slack15.0\.txz$/{print$9}')"
+
+if [ -n "${slackware_ca}" ]
+then
+  echo "[*] Downloading CA package from Slackware"
+  wget -N \
+    "ftp://ftp.slackware.com/pub/slackware/slackware64-15.0/patches/packages/${slackware_ca}" \
+    "ftp://ftp.slackware.com/pub/slackware/slackware64-15.0/patches/packages/${slackware_ca}.asc"
+  gpg --keyring trustedkeys.kbx --no-default-keyring --import 0<<-EOF
+	-----BEGIN PGP PUBLIC KEY BLOCK-----
+	Version: GnuPG v1.4.12 (GNU/Linux)
+	
+	mQGiBD5dIFQRBADB31WinbXdaGk/8RNkpnZclu1w3Xmd5ItACDLB2FhOhArw35EA
+	MOYzxI0gRtDNWN4pn9n74q4HbFzyRWElThWRtBTYLEpImzrk7HYVCjMxjw5A0fTr
+	88aiHOth5aS0vPAoq+3TYn6JDSipf2bR03G2JVwgj3Iu066pX4naivNm8wCgldHG
+	F3y9vT3UPYh3QFgEUlCalt0D/3n6NopRYy0hMN6BPu+NarXwv6NQ9g0GV5FNjEEr
+	igkrD/htqCyWAUl8zyCKKUFZZx4UGBRZ5guCdNzwgYH3yn3aVMhJYQ6tcSlLsj3f
+	JIz4LAZ3+rI77rbn7gHHdp7CSAuV+QHv3aNanUD/KGz5SPSvF4w+5qRM4PfPNT1h
+	LMV8BACzxiyX7vzeE4ZxNYvcuCtv0mvEHl9yD66NFA35RvXaO0QiRVYeoUa5JOQZ
+	gwq+fIB0zgsEYDhXFkC1hM/QL4NccMRk8C09nFn4eiz4dAEnwKt4rLCJKhkLl1DW
+	TSoXHe/dOXaLnFyLzB1J8hEYmUvw3SwPt//wMqDiVBLeZfFcdLQwU2xhY2t3YXJl
+	IExpbnV4IFByb2plY3QgPHNlY3VyaXR5QHNsYWNrd2FyZS5jb20+iF8EExECAB8E
+	CwcDAgMVAgMDFgIBAh4BAheABQJQPlypBQlBo7MrAAoJEGpEY8BAECIzjOwAn3vp
+	tb6K1v2wLI9eVlnCdx4m1btpAJ9sFt4KwJrEdiO5wFC4xe9G4eZl4rkBDQQ+XSBV
+	EAQA3VYlpPyRKdOKoM6t1SwNG0YgVFSvxy/eiratBf7misDBsJeH86Pf8H9OfVHO
+	cqscLiC+iqvDgqeTUX9vASjlnvcoS/3H5TDPlxiifIDggqd2euNtJ8+lyXRBV6yP
+	sBIA6zki9cR4zphe48hKpSsDfj7uL5sfyc2UmKKboSu3x7cAAwUD/1jmoLQs9bIt
+	bTosoy+5+Uzrl0ShRlv+iZV8RPzAMFuRJNxUJkUmmThowtXRaPKFI9AVd+pP44aA
+	J+zxCPtS2isiW20AxubJoBPpXcVatJWi4sG+TM5Z5VRoLg7tIDNVWsyHGXPAhIG2
+	Y8Z1kyWwb4P8A/W2b1ZCqS7Fx4yEhTikiEwEGBECAAwFAlA+XL8FCUGjs2IACgkQ
+	akRjwEAQIjMsbQCgk59KFTbTlZfJ6FoZjjEmK3/xGR4AniYT+EdSdvEyRtZYkqWz
+	p1ayvO1b
+	=tibb
+	-----END PGP PUBLIC KEY BLOCK-----
+EOF
+  echo "[*] Verifying CA package's PGP signature"
+  if ! gpgv "${slackware_ca}.asc" "${slackware_ca}"
+  then
+    echo -e "[\033[1;31m-\033[0m] Could not verify CA package's PGP signature" 1>&2
+    exit 1
+  fi
+  echo "[*] Extracting CAs"
+  tar xvf "${slackware_ca}" usr/share/ca-certificates/mozilla
+  ca_dir="./usr/share/ca-certificates"
+  echo "[+] Using Slackware's CAs"
+fi
 
 while read -r
 do
-  if [ ! -f "/usr/share/ca-certificates/${REPLY}" ]
+  if [ ! -f "${ca_dir}/${REPLY}" ]
   then
     echo -e "[\033[1;31m-\033[0m] CA \`${REPLY}' does not exist" 1>&2
     exit 1
   fi
-  cat "/usr/share/ca-certificates/${REPLY}"
+  cat "${ca_dir}/${REPLY}"
 done 0<files/ca-certificates.conf.new 1>"${ca_file}"
 
 # HTTPS
